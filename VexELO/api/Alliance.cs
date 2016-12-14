b74aaf6aae7972aee07788d14dfbd6c7bc554aa0
Kevin <dragn194@gmail.com>
2016-12-13 23:20:54 -0500

    Add a constructor to the Alliance struct

--- VexELO/api/Alliance.cs
+++ VexELO/api/Alliance.cs
@@ -1,5 +1,12 @@
 ﻿struct Alliance
 {
+
+    public Alliance(string teamCode1, string teamCode2)
+    {
+        TeamCode1 = teamCode1;
+        TeamCode2 = teamCode2;
+    }
+    
     public string TeamCode1 { get; set; }
     public string TeamCode2 { get; set; }
 }
